363a9bcfbb547cf7149b7d3d9adb5324d4d4d1f9
Kartikeya Sharma <kartikeyasharma@Kartikeyas-MacBook-Air.local>
2025-11-02 23:22:44 +0530

    Handled case of account already existing in register handler

--- handlers/register.go
+++ handlers/register.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/Kartikeya2710/to-do/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,6 +18,28 @@ func (h *AuthHandlers) Register(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		h.logger.Printf("Error decoding the user from request: %v", err)
 		http.Error(w, "Invalid payload", http.StatusBadRequest)
+
+		return
+	}
+
+	filter := bson.D{{Key: "email", Value: user.Email}}
+	var resultUser models.User
+
+	err := h.collection.FindOne(r.Context(), filter).Decode(&resultUser)
+	// we found a user with the same email
+	if err == nil {
+		h.logger.Printf("User with email %s found in database", user.Email)
+
+		w.WriteHeader(http.StatusConflict)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error":   "EmailAlreadyRegistered",
+			"message": "An account with this email already exists. Please log in or reset your password.",
+		})
+
+		return
+	} else if !errors.Is(err, mongo.ErrNoDocuments) {
+		h.logger.Printf("Error finding user with email: %s", user.Email)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 
 		return
 	}
